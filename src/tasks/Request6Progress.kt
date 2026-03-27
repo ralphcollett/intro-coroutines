906475a517453e6be5b0508194e60e186137a53d
ralph.collett <ralph.collett@gmail.com>
2026-03-27 09:27:29 +0000

    Implement progress.

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -1,11 +1,26 @@
 package tasks
 
 import contributors.*
+import kotlin.collections.fold
 
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    repos.withIndex().fold(emptyList<User>()) { acc, (index, repo) ->
+        val progress =
+            (acc + service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+            ).aggregate()
+
+        updateResults(progress, index == repos.lastIndex)
+        progress
+    }
 }
