c5cd3f972c1bc94db4e89087cd17cf5ec2d77378
ralph.collett <ralph.collett@gmail.com>
2026-03-27 09:40:48 +0000

    Test channels.

--- test/tasks/Request7ChannelsKtTest.kt
+++ test/tasks/Request7ChannelsKtTest.kt
@@ -3,25 +3,27 @@ package tasks
 import contributors.MockGithubService
 import contributors.concurrentProgressResults
 import contributors.testRequestData
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.currentTime
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Test
 
 class Request7ChannelsKtTest {
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun testChannels() = runBlocking {
-        val startTime = System.currentTimeMillis()
+    fun testChannels() = runTest {
+        val startTime = currentTime
         var index = 0
-        loadContributorsChannels(MockGithubService, testRequestData) {
-                users, _ ->
+        loadContributorsChannels(MockGithubService, testRequestData) { users, _ ->
             val expected = concurrentProgressResults[index++]
-            val time = System.currentTimeMillis() - startTime
-            /*
-            // TODO: uncomment this assertion
-            Assert.assertEquals("Expected intermediate result after virtual ${expected.timeFromStart} ms:",
-                expected.timeFromStart, time)
-            */
-            Assert.assertEquals("Wrong intermediate result after $time:", expected.users, users)
+            val time = currentTime - startTime
+            Assert.assertEquals(
+                "Expected intermediate results after ${expected.timeFromStart} ms:",
+                expected.timeFromStart, time
+            )
+            Assert.assertEquals("Wrong intermediate results after $time:", expected.users, users)
         }
     }
 }
